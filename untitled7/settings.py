88baeddd326140891e088eadce2ff88846f9bc86
BekoevPavel <48162243+BekoevPavel@users.noreply.github.com>
2019-11-26 02:46:33 +0300

    Update settings.py

--- untitled7/settings.py
+++ untitled7/settings.py
@@ -123,7 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
