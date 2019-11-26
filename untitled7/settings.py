0ee70b806718052fedad1d86207aa157f48690f7
Pavel Bekoev <pablank@bk.ru>
2019-11-26 03:06:16 +0300

    Added files and changes required for deployment to heroku

--- untitled7/settings.py
+++ untitled7/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
